a2113926c30dbf651806b87ea6b307fe4f9136c7
Quentin Diebold <quentus123@gmail.com>
2024-07-12 16:16:36 +0000

    rewrote compile script for ubuntu

---
+++ compile_wasm.sh
@@ -0,0 +1,24 @@
+TARGET="Adder"
+SCENARIO_JSON_EXECUTABLE="/home/Quentin/multiversx-sdk/vmtools/v1.5.24/mx-chain-vm-go-1.5.24/cmd/test/test"
+
+# Do not edit the below variables
+TARGET_PACKAGE_PATH="$(pwd)/Examples/$TARGET"
+TARGET_PACKAGE_OUTPUT_PATH="$TARGET_PACKAGE_PATH/output"
+OBJECT_FILE_PATH="$(pwd)/$TARGET.o"
+WASM_BUILT_FILE_PATH="$(pwd)/$TARGET.wasm"
+WASM_OPT_FILE_PATH="$(pwd)/$TARGET-opt.wasm"
+WASM_DEST_FILE_PATH="$TARGET_PACKAGE_OUTPUT_PATH/$TARGET.wasm"
+SCENARIOS_JSON_DIR="$TARGET_PACKAGE_PATH/scenarios"
+
+rm -rf .build || true
+rm -rf *.o || true
+rm -rf *.d || true
+
+SWIFT_WASM=true swift build --target $TARGET --triple wasm32-unknown-none-wasm --disable-index-store -Xswiftc -Osize -Xswiftc -gnone
+wasm-ld --no-entry --allow-undefined $OBJECT_FILE_PATH -o $WASM_BUILT_FILE_PATH
+wasm-opt -Os -o $WASM_OPT_FILE_PATH $WASM_BUILT_FILE_PATH
+
+mkdir -p $TARGET_PACKAGE_OUTPUT_PATH
+cp $WASM_OPT_FILE_PATH $WASM_DEST_FILE_PATH
+
+$SCENARIO_JSON_EXECUTABLE $SCENARIOS_JSON_DIR
