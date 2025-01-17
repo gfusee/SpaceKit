dfa3448c4101e59206fb892050bc8d0f7890076b
quentin <quentus123@gmail.com>
2025-01-17 19:07:31 +0100

    wasm tests

--- Examples/FeatureTests/TokenOperations/TestTokenOperationsController.swift
+++ Examples/FeatureTests/TokenOperations/TestTokenOperationsController.swift
@@ -25,15 +25,17 @@ import SpaceKit
     public func createNFT(
         tokenIdentifier: Buffer,
         initialSupply: BigUint,
+        royalties: BigUint,
+        attributes: Buffer,
         to: Address
     ) -> UInt64 {
         let nonce = Blockchain.createNft(
             tokenIdentifier: tokenIdentifier,
             amount: initialSupply,
             name: "NFT",
-            royalties: 0,
+            royalties: royalties,
             hash: "",
-            attributes: Buffer(),
+            attributes: attributes,
             uris: Vector()
         )
        
@@ -58,5 +60,49 @@ import SpaceKit
             nonce: nonce,
             amount: amount
         ).burn()
+    }
+    
+    public func modifyTokenRoyalties(
+        tokenIdentifier: Buffer,
+        nonce: UInt64,
+        royalties: UInt64
+    ) {
+        Blockchain.modifyTokenRoyalties(
+            tokenIdentifier: tokenIdentifier,
+            nonce: nonce,
+            royalties: royalties
+        )
+    }
+    
+    public func getTokenRoyalties(
+        tokenIdentifier: Buffer,
+        nonce: UInt64
+    ) -> BigUint {
+        Blockchain.getTokenRoyalties(
+            tokenIdentifier: tokenIdentifier,
+            nonce: nonce
+        )
+    }
+    
+    public func updateNftAttributes(
+        tokenIdentifier: Buffer,
+        nonce: UInt64,
+        attributes: Buffer
+    ) {
+        Blockchain.updateNftAttributes(
+            tokenIdentifier: tokenIdentifier,
+            nonce: nonce,
+            attributes: attributes
+        )
+    }
+    
+    public func getTokenAttributes(
+        tokenIdentifier: Buffer,
+        nonce: UInt64
+    ) -> Buffer {
+        Blockchain.getTokenAttributes(
+            tokenIdentifier: tokenIdentifier,
+            nonce: nonce
+        )
     }
 }
