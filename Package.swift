c0fba7f27a751b43d0f0218034d8e38447f6fd57
quentin <quentus123@gmail.com>
2025-02-25 04:20:00 +0100

    fixes

--- Package.swift
+++ Package.swift
@@ -63,7 +63,7 @@ if !isWasm {
     packageDependencies.append(contentsOf: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
-        .package(url: "https://github.com/swiftlang/swift-package-manager", from: "0.6.1"),
+        .package(url: "https://github.com/swiftlang/swift-package-manager", from: "0.6.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
     ])
     
