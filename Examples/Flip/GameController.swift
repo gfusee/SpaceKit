751f0a3498883e5e3bf9cf9eb59502b9d7a7cd33
quentin <quentus123@gmail.com>
2025-03-10 17:42:34 +0100

    progresses

--- Examples/Flip/GameController.swift
+++ Examples/Flip/GameController.swift
@@ -6,28 +6,96 @@ let HUNDRED_PERCENT: UInt64 = 100_000
     public func flip() {
         let payment = Message.singleEsdt
         
-        let tokenReserve = StorageController()
+        let storageController = StorageController()
+        
+        let tokenReserve = storageController
             .getTokenReserve(
                 tokenIdentifier: payment.tokenIdentifier,
                 tokenNonce: payment.nonce
             )
             .get()
         
-        let maximumBet = StorageController()
+        let maximumBet = storageController
             .getMaximumBet(
                 tokenIdentifier: payment.tokenIdentifier,
                 tokenNonce: payment.nonce
             )
             .get()
         
-        let maximumBetPercent = StorageController()
+        let maximumBetPercent = storageController
             .getMaximumBetPercent(
                 tokenIdentifier: payment.tokenIdentifier,
                 tokenNonce: payment.nonce
             )
             .get()
 
-        let maximumBetPercentComputed = tokenReserve * BigUint(value: maximumBetPercent) / BigUint(value: HUNDRED_PERCENT)
-        let maximumAllowedBet =
+        let hundred_percent = BigUint(value: HUNDRED_PERCENT)
+        
+        let maximumBetPercentComputed = tokenReserve * BigUint(value: maximumBetPercent) / hundred_percent
+        let maximumAllowedBet = maximumBet.min(other: maximumBetPercentComputed)
+        
+        let ownerFees = payment.amount * BigUint(value: storageController.ownerPercentFees) / hundred_percent
+        let bounty = payment.amount * BigUint(value: storageController.bountyPercentFees) / hundred_percent
+        let amount = payment.amount - ownerFees - bounty
+        
+        require(
+            amount <= maximumAllowedBet,
+            "Bet is too high. Maximum allowed bet: \(maximumAllowedBet)"
+        )
+        
+        let lastFlipId = storageController.lastFlipId
+        let flipId = lastFlipId + 1
+        
+        let flip = Flip(
+            id: flipId,
+            playerAddress: Message.caller,
+            tokenIdentifier: payment.tokenIdentifier,
+            tokenNonce: payment.nonce,
+            amount: amount,
+            bounty: bounty,
+            blockNonce: Blockchain.getBlockNonce(),
+            minimumBlockBounty: storageController.minimumBlockBounty
+        )
+    }
+    
+    public func bounty() {
+        let caller = Message.caller
+        
+        require(Blockchain.isS, <#T##errorMessage: Buffer##Buffer#>)
+    }
+    
+    private func makeFlip(
+        bountyAddress: Address,
+        flip: Flip
+    ) {
+        let randomNumber = Randomness.nextUInt8InRange(min: 0, max: 2)
+        let isWin = randomNumber == 1
+        
+        bountyAddress.send(
+            tokenIdentifier: flip.tokenIdentifier,
+            nonce: flip.blockNonce,
+            amount: flip.amount
+        )
+        
+        let profitIfWin = flip.amount * 2
+        
+        var storageController = StorageController()
+        
+        if isWin {
+            flip.playerAddress
+                .send(
+                    tokenIdentifier: flip.tokenIdentifier,
+                    nonce: flip.tokenNonce,
+                    amount: profitIfWin
+                )
+        } else {
+            _ = storageController.getTokenReserve(
+                tokenIdentifier: flip.tokenIdentifier,
+                tokenNonce: flip.tokenNonce
+            )
+            .update { $0 + profitIfWin }
+        }
+        
+        storageController.$flipForId[flip.id].clear()
     }
 }
